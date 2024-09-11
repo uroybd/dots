629592f9aabb02304e09125a4c65627445ea0a54
Utsob Roy <roy@utsob.me>
2024-09-11 10:34:19 +0600

    fix(neovim): conform fix

--- dotfiles/config/nvim/lua/user/extras/conform.lua
+++ dotfiles/config/nvim/lua/user/extras/conform.lua
@@ -1,22 +1,28 @@
 local M = {
-"stevearc/conform.nvim"
+  "stevearc/conform.nvim",
 }
 
 function M.config()
-require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- Conform will run multiple formatters sequentially
-    python = { "isort", "black" },
-    -- Use a sub-list to run only the first available formatter
-    javascript = { { "prettierd", "prettier" } },
-  },
-format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
-})
+  require("conform").setup {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      -- Conform will run multiple formatters sequentially
+      python = { "isort", "black" },
+      -- Use a sub-list to run only the first available formatter
+      javascript = { { "prettierd", "prettier" } },
+    },
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      lsp_format = "fallback",
+      timeout_ms = 1000,
+    },
+  }
+end
+function M.init()
+  vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 end
 
 return M
